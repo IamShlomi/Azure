c0f85bea8c597349cc867a530465d09515a1a0d0
IamShlomi <43221312+IamShlomi@users.noreply.github.com>
2020-07-30 15:45:46 +0300

    Add files via upload

---
+++ StartStopVMsByTags/StartStopVMs.ps1
@@ -0,0 +1,37 @@
+﻿# Start VMs Script
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave –Scope Process
+
+# Connect to Automation Account & Set parameters
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+$AzureContext = Select-AzSubscription -SubscriptionId $Conn.SubscriptionID
+$resources = Get-AzVM
+
+# Start VM Action for Patch Management Pool #1
+foreach ($r in $resources){
+    if ($r.Tags.PatchManagementPool -Like '1'){
+        Start-AzVM -Name $r.'Name' -ResourceGroupName '<ResourceGroupName>' -AzContext $AzureContext
+    }
+}
+
+#################################################################################
+
+# Stop VMs Script
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave –Scope Process
+
+# Connect to Automation Account & Set parameters
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Connect-AzAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+$AzureContext = Select-AzSubscription -SubscriptionId $Conn.SubscriptionID
+$resources = Get-AzVM
+
+# Stop VM Action for Patch Management Pool #1
+foreach ($r in $resources){
+    if ($r.Tags.PatchManagementPool -Like '1'){
+        Stop-AzVM -Name $r.'Name' -ResourceGroupName '<ResourceGroupName>' -force
+    }
+}
---
+++ TagVMs/AddVMTagsFromCSV.ps1
@@ -0,0 +1,10 @@
+﻿$vmFile = import-csv -Path "C:\Users\User\Desktop\IML-Local\Cloud Update Management\Azure\IML-Azure VMs-test.csv"
+$ResourceGroup = 'production-network_rg'
+
+ForEach($resource in $vmFile){
+
+    $tags = @{"Application" = $resource.Application; "OSType" = $resource.OSType; "Environment" = $resource.Environment; "Role" = $resource.Role; "Stack" = $resource.'Stack'; "PCI" = $resource.'PCI'}
+
+    $Cloudresource = Get-AzResource -Name $resource.'Name' -ResourceGroup $ResourceGroup
+    Update-AzTag -ResourceId $Cloudresource.id -Tag $tags -Operation Merge
+}
